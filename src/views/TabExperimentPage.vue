fc9240ae106ec73385f01f797c75a202d5689da9
ophalvens <ophalvens@gmail.com>
2025-10-20 17:48:17 +0200

    Geolocation code

--- src/views/TabExperimentPage.vue
+++ src/views/TabExperimentPage.vue
@@ -29,7 +29,13 @@
 					<ion-toggle :checked="false" name="discordMode" class="discord">Discord mode <ion-icon
 							:icon="logoDiscord"></ion-icon></ion-toggle>
 				</ion-item>
-
+				<ion-item>
+					<ion-button @click="askPermission" :disabled="geoEnabled">Enable Geolocation</ion-button><br>
+					<ion-button @click="getLocation" :disabled="geoDisabled">Haal locatie op</ion-button>
+				</ion-item>
+				<ion-item>
+					<ion-label>lat : {{ coords.latitude }} <br> lon : {{ coords.longitude }}</ion-label>
+				</ion-item>
 
 			</ion-list>
 
@@ -39,10 +45,38 @@
 
 <script setup>
 import { ref } from 'vue'
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonIcon, IonToggle, onIonViewDidEnter, onIonViewDidLeave, onIonViewWillEnter, onIonViewWillLeave } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonIcon, IonToggle, onIonViewDidEnter, onIonViewDidLeave, onIonViewWillEnter, onIonViewWillLeave, IonButton } from '@ionic/vue';
 import { colorWand, cafe, logoDiscord } from 'ionicons/icons';
+import { Geolocation } from '@capacitor/geolocation';
 
 const darkmode = ref(false);
+const coords = ref({ latitude: 4, longitude: 50 });
+const geoEnabled = ref(false);
+const geoDisabled = ref(true);
+
+const askPermission = async () => {
+	// vraag voor de location permission
+	const perm = await Geolocation.requestPermissions({ permissions: ["location"] });
+	console.log("permission :");
+	console.log(perm);
+	if (perm.location == 'granted') {
+		geoDisabled.value = false;
+		geoEnabled.value = true;
+	}
+};
+
+const getLocation = async () => {
+	// gaal de locatie op;
+
+	let coordinates = await Geolocation.getCurrentPosition(
+		{
+			enableHighAccuracy: true,
+			timeout: 10000,
+			maximumAge: 0
+		});
+	coords.value.latitude = coordinates.coords.latitude;
+	coords.value.longitude = coordinates.coords.longitude;
+};
 
 const toggleDarkMode = () => {
 	console.log(`darkmode : ${darkmode.value}`)
